8140d994d4b8f4140373eb5caf052d08ab91870f
galexbh <galexbh@protonmail.com>
2020-12-21 20:21:01 -0600

    feat:rss parser

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,7 @@
-require('dotenv/config')
+/**
+ * @see https://www.npmjs.com/package/dotenv
+ */
+require('dotenv').config()
 
 /**
  * @see https://telegraf.js.org/#/
@@ -7,12 +10,24 @@ const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+/**
+ * @see https://www.npmjs.com/package/rss-parser
+ */
+const Parser = require('rss-parser')
+const parser = new Parser()
+
 bot.start((ctx) => ctx.reply('hola'))
 
 bot.help((ctx =>{
     console.log(ctx)
     ctx.reply('ayuda')
-}))
+}));
+
+(async () => {
+
+    const {items} = await parser.parseURL('https://www.muylinux.com/feed/')
+    const [{title,description,link}] = items
+    console.log(`${title}\n,${description}\n,${link}`)
+  })();
 
 bot.launch()
-
