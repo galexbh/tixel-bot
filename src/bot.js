01b6d29f3bf4438b0ece64b13d4d1080fc68b9df
galexbh <galexbh@protonmail.com>
2021-05-13 19:02:43 -0600

    feat:start

--- src/bot.js
+++ src/bot.js
@@ -11,9 +11,12 @@ const {Telegraf} = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const rss = require("./rssFeed");
-const channel = require("./channel")
+const channel = require("./channel");
 
-bot.start((ctx) => ctx.reply("hola"));
+bot.start((ctx) => {
+  ctx.reply("Hi, I'm TixelBot. In charge of sending rss to the channel https://t.me/TixelChannel.");
+  ctx.reply("I'm open source and you can check it out at https://github.com/galexbh/tixel-bot.");
+});
 
 bot.help((ctx) => {
   console.log(ctx);
--- src/channel.js
+++ src/channel.js
@@ -4,6 +4,10 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const chatId = process.env.CHANNEL_ID;
 
+/**
+ * @param {string} msg
+ *
+ */
 const sendMessageToChannel = (msg) => {
   bot.telegram.sendMessage(chatId, msg);
 }
